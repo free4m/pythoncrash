155cf5f1b92879d9f250bb64dfdec19c46f74398
Chris Barnes <chgbarnes@gmail.com>
2017-05-22 21:54:58 -0500

    first stuff added

---
+++ python_works/name.py
@@ -0,0 +1,19 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print("Hello, " + full_name.title() + "!")
+
+print("Python")
+print("\tPython")
+
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
